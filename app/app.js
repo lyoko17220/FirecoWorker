87e5fc475df24b16332000adad67079928bebb63
Unknown <mozombel@orange.fr>
2018-03-26 11:50:15 +0200

    integration front
    regroupement avec le front

--- app/app.js
+++ app/app.js
@@ -4,15 +4,23 @@ const express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
 	port = process.env.PORT_WORKER || 8082,
-	api = require('./routes/api');
+	view = require('./routes/view'),
+	api = require('./routes/api'),
+	nunjucks = require('nunjucks');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) =>{
-	res.send('Erreur 418', 418);
+	res.status(301).redirect("/view")
 });
 
 app.use('/api', api);
+app.use('/view', view);
+
+nunjucks.configure('dashboard/page', {
+	autoescape: true,
+	express: app
+});
 
 app.listen(port, () => {
 	console.log('API started on port : ' + port);
---
+++ app/routes/view.js
@@ -0,0 +1,35 @@
+const express = require('express'),
+    view = express.Router(),
+	path = require('path');
+
+view.use('/css', express.static(__dirname + '/../node_modules/bootstrap/dist/css'));
+view.use('/ressources', express.static(__dirname + '/../dashboard/ressources'));
+
+view.get('/', function(req, res){
+	res.render('homePage.html');
+});
+
+view.get('/navbar', function(req, res){
+	res.render('layout.html');
+});
+
+view.get('/table', function(req, res){
+	res.render('table.html');
+});
+
+view.get('/login', function (req, res){
+	res.render('login.html');
+
+});
+
+view.get('/home', function (req, res){
+	res.render('homePage.html');
+
+});
+
+view.get('/subscription', function (req, res){
+	res.render('/page/subscription.html');
+
+});
+
+module.exports = view;
