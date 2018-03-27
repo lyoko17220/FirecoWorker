d41897f7c5adcc996ff6c63ff9d10e7e0fb92a36
Unknown <anthony.marechal@yahoo.fr>
2018-03-27 16:39:08 +0200

    update: API lister contenu d'un dossier + download dinamique

--- app/routes/api/files.js
+++ app/routes/api/files.js
@@ -29,13 +29,15 @@ files.get('/upload/request/:user_token', (req, res) =>{
 	});
 });
 
-files.get('/download/request/:user_token', (req, res) =>{
+files.post('/download/request/:user_token', (req, res) =>{
 	Users.findOne({'token': req.params.user_token}).then((doc) => {
 		if (doc) {
 			let download = new Download();
 			let date = new Date().getTime();
 
 			download.user_token = doc._id;
+			download.folder = req.body.folder;
+			download.filename = req.body.filename;
 			download.token = token.generate(16);
 			download.period_start = date;
 			download.period_end = date + 604800000;
@@ -57,7 +59,7 @@ files.get('/download/:user_token/:download_token', (req, res) =>{
 		if (doc) {
 			Download.findOne({'token': req.params.download_token, 'period_end': {$gt : new Date()}}).then((doc2) =>{
 				if (doc2) {
-					const folder = doc2.folder,
+					const folder = '/firecodata' + doc2.folder,
 						filename = doc2.filename;
 
 					res.download(folder + '/' + filename, filename);
@@ -71,18 +73,34 @@ files.get('/download/:user_token/:download_token', (req, res) =>{
 	});
 });
 
-files.get('/upload/:user_token/:upload_token', (req, res) =>{
+files.post('/upload/:user_token/:upload_token', (req, res) =>{
 	Users.findOne({'token': req.params.user_token}).then((doc) => {
 		if (doc) {
 			Upload.findOne({'token': req.params.upload_token}).then((doc2) =>{
 				if (doc2){
+					const Storage = multer.diskStorage({
+						destination: (req, file, callback) =>{
+							callback(null, './' + req.document.destination_folder);
+						},
+						filename: (req, file, callback) =>{
+							callback(null, file.fieldname + '_' + Date.now() + '_' + file.originalname);
+						}
+					});
+					const upload = multer({
+						storage: Storage
+					}).array('file_uploader', 1);
 
+					upload(req, res, (err) =>{
+						if(err)
+							res.json({message: 'Impossible de téléverser le fichier'});
+						res.json({message: 'Le fichier à bien été téléversé'});
+					});
 				}else{
 					res.status(408).send('Délais d\'attente dépassé.');
 				}
 			});
 		}else{
-			res.status(401).send('Vous devez être connecté pour accéder à cette ressouce.');
+			res.status(401).send('Une authentification est requise pour effectuer cette action.');
 		}
 	});
 });
--- app/routes/api/folders.js
+++ app/routes/api/folders.js
@@ -1,19 +1,52 @@
 const express = require('express'),
-	folders = express.Router();
+	folders = express.Router(),
+	fs = require('fs'),
+	path = require('path'),
+	Users = require('../../db/schemas/users');
 
-folders.post('/create', (req, res) =>{
+folders.post('/create', (req, res) => {
 	res.json('API destinee a la creation de dossiers');
 });
 
-folders.delete('/delete', (req, res) =>{
+folders.delete('/delete', (req, res) => {
 	res.json('API destinee a la suppression de dossiers');
 });
 
-folders.put('/rename', (req, res) =>{
+folders.put('/rename', (req, res) => {
 	res.json('API destinee pour renommer les dossiers');
 });
 
-folders.get('/', (req, res) =>{
+folders.post('/content/:user_token', (req, res) => {
+	Users.findOne({'token': req.params.user_token}).then((doc) => {
+		if (doc) {
+			let output = {content: []};
+			let folderpath = '/firecodata' + req.body.folder;
+			fs.readdir(folderpath, (err, files) => {
+				if (err) {
+					res.status(400).json('Le dossier n\'existe pas ou le chemin d\'accès est incorrect.');
+				} else {
+					files.forEach(file => {
+						let type = 'folder';
+						let realpath = path.join(folderpath, file);
+						if (fs.statSync(realpath).isFile())
+							type = 'file';
+
+						output.content.push({
+							'name': file,
+							type,
+							'path': folderpath
+						});
+					});
+					res.json(output);
+				}
+			});
+		} else {
+			res.status(401).json({message: 'Une authentification est requise pour effectuer cette action.'});
+		}
+	});
+});
+
+folders.get('/', (req, res) => {
 	res.send('Erreur 418', 418);
 });
 
