0db21b6fedd8f48eb259ccd520a88071cf8dc248
Nicolas S <sagot.nicolas@outlook.fr>
2018-03-29 10:54:05 +0200

    fix: eslint + script.js

---
+++ app/dashboard/ressources/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+	/**
+     * Inscription
+	 */
+	$('#sign').click(function (e) {
+		e.preventDefault();
+		$.ajax({
+			url: '/api/users/sign',
+			type: 'POST',
+			data: {
+				username: $('#username').val(),
+				password: $('#password').val(),
+				firstname: $('#firstname').val(),
+				lastname: $('#lastname').val()
+			}
+		});
+	});
+
+	/**
+     * Connexion
+	 */
+	$('#login').click(function (e) {
+		e.preventDefault();
+		$.ajax({
+			url: '/api/users/login',
+			type: 'POST',
+			data:{
+				username: $('#username').val(),
+				password: $('#password').val(),
+			},
+			dataType: 'json',
+			success: function (data) {
+				console.log(data);
+			},
+			error: function (data) {
+				$('#alert').html(`<div id="alert" class="alert alert-dismissable alert-danger fade show" >
+					<span> Le nom d'utilisateur ou le mot de passe ne correspond pas.</span>
+					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button></div>`);
+			}
+		});
+	});
+
+});
